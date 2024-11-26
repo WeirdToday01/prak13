175a8a6cfde92153a3559da7aec49643ba674614
WeirdToday01 <sergenator1000@mail.ru>
2024-11-26 14:32:01 +0200

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static int Meters(int k, int m)
+        {
+            return k * 1000 + m;
+        }
+
+        static void Main()
+        {
+            int result1 = Meters(3, 40);
+            Console.WriteLine($"3 км 40 м = {result1} метров");
+
+            int result2 = Meters(12, 540);
+            Console.WriteLine($"12 км 540 м = {result2} метров");
+        }
+    }
+}
