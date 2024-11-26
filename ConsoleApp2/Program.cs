2827a640225d6c19553622e4d952d2237e150eab
WeirdToday01 <sergenator1000@mail.ru>
2024-11-26 14:29:58 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+
+    /// <summary>
+    /// Найти минимальную величину их трех целых переменных a, b, c с использованием вспомогательных методов (ввода аргументов, нахождения минимальной величины, вывода результата).
+    /// </summary>
+    class Program
+    {
+        //task 2
+        static int InputNum(string sapros)
+        
+            {
+                Console.Write(sapros);
+                return int.Parse(Console.ReadLine());
+            }
+
+        static int GetMin(int a, int b)
+        {
+            return a < b ? a : b;
+        }
+
+        static void OutputResult(int min)
+        {
+            Console.WriteLine("Минимальное значение: " + min);
+        }
+
+        static void Main()
+        {
+            int a = InputNum("Введите первое число: ");
+            int b = InputNum("Введите второе число: ");
+            int c = InputNum("Введите третье число: ");
+
+            int min = GetMin(GetMin(a, b),c);
+
+            OutputResult(min);
+        }
+
+    }
+}
