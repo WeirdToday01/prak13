e4da51e5756402cd3e7e46b819b966ed431f466d
WeirdToday01 <sergenator1000@mail.ru>
2024-11-26 14:32:30 +0200

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+
+
+    /// <summary>
+    /// Определить статический метод void Numbers (int n), который выводит на экран четные числа
+    /// от 2 до числа n. Используя этот метод, вывести на экран четные числа: а) от 2 до 20;    б) от 2 до 50;    в) от 2 до m (m вводится с клавиатуры).
+    /// </summary>
+    class Program
+    {
+        //task 6
+        public static void Numbers(int n)
+        {
+            for (int i = 2; i <= n; i += 2)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main()
+        {
+            Numbers(20);
+
+            Console.Write("Введите число m: ");
+            int m = int.Parse(Console.ReadLine());
+            Numbers(m);
+        }
+    }
+}
